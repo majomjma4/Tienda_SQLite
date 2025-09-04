2233abfca1742975ec309ea87f77f5d8c7af291c
Majo <monterosmarijo2004@gmil.com>
2025-09-04 18:29:51 -0500

    Cambio de funciones del la compra final y ver compras

--- tienda_sqlite.py
+++ tienda_sqlite.py
@@ -25,7 +25,7 @@ def inicializar_bd():
                     nombre     TEXT,
                     precio     REAL,
                     cantidad   INTEGER,
-                    FOREING KEY (codigo) REFERENCES catalogo(codigo)
+                    FOREIGN KEY (codigo) REFERENCES catalogo(codigo)
                       
                 )""")
         
@@ -46,7 +46,7 @@ def ver_catalogo():
         if not productos: print("Catálogo vació. "); return
         print("\n------ CATÁLOGO ------")
         for codigo, nombre, precio, stock  in productos:
-            print(f"{'codigo'} | {'nombre'} | ${'precio':.2f} | {'stock'} ")
+            print(f"{codigo} | {nombre} | ${precio:.2f} | {stock} ")
       
 def agregar_producto():
     codigo = input("Ingrese el código del producto: ").strip().upper()
@@ -86,7 +86,7 @@ def agregar_carrito():
         try: cantidad = int(input("Ingrese la cantidad a comprar: ").strip()); assert cantidad>0    
         except: print("Cantidad inválida. Debe ser un número entero mayor a 0."); return
             
-        if cantidad > stock: print(f"Stock insuficiente. Disponible: {producto['stock']}"); return
+        if cantidad > stock: print(f"Stock insuficiente. Disponible: {stock}"); return
         
         c.execute("SELECT cantidad FROM carrito WHERE codigo =?", (codigo,))
         item = c.fetchone()
@@ -98,13 +98,13 @@ def agregar_carrito():
         
         c.execute("UPDATE catalogo SET stock = stock - ? WHERE codigo =?", (cantidad, codigo))
         conn.commit()
-        print(f"{cantidad} unidades de {producto['nombre']} ha sido agregado al carrito")
+        print(f"{cantidad} unidades de {nombre} ha sido agregado al carrito")
        
 def ver_carrito():
     with conectar() as conn: 
         c= conn.cursor()
         c.execute("SELECT nombre, precio, cantidad FROM carrito")
-        carrito = c.fetchone()           
+        carrito = c.fetchall()           
         if not carrito: print("Carrito vacío"); return
         
         total = 0
@@ -112,54 +112,59 @@ def ver_carrito():
         for nombre, precio, cantidad in carrito:
             subtotal = precio * cantidad
             total += subtotal
-            print(f"{nombre} x {'cantidad'} = ${subtotal:.2f}")
+            print(f"{nombre} x {cantidad} = ${subtotal:.2f}")
         print(f"Total: ${total:.2f}")
         
 def finalizar_compra():
-    cargar_carrito()
-    if not carrito: print("Carrito vacío"); return
-    if input("Desea finalizar la compra (si/no): ").strip().lower()!= 'si': print("Compra cancelada"); return
+    with conectar() as conn:
+        c = conn.cursor()
+        c.execute("SELECT codigo, nombre, precio, cantidad FROM carrito")
+        carrito = c.fetchall()
+        if not carrito: print("Carrito vacío"); return
+        if input("Desea finalizar la compra (si/no): ").strip().lower()!= 'si': print("Compra cancelada"); return
    
-    total = sum(car['precio'] * car['cantidad'] for car in carrito)
-    iva = total * 0.15; 
-    subtotal_iva = total + iva
-    descuento = 0.10 if subtotal_iva > 50 else 0.05 if subtotal_iva > 20 else 0
-    total_final = subtotal_iva * (1-descuento)
-    
-    ticket = f"\n{'='*40}\nFACTURA \nFecha: {datetime.now()}\n {"-" * 40}\n"
-    for car in carrito:
-        subtotal_producto = car['precio'] * car['cantidad']
-        subtotal_producto_iva = subtotal_producto * 1.15
-        ticket += f"{car['nombre']} x {car['cantidad']} = ${subtotal_producto:.2f}\n "
+        total_sin_iva = sum(precio * cantidad for _, _, precio, cantidad in carrito)
+        iva = total_sin_iva * 0.15; 
+        subtotal_iva = total_sin_iva + iva
+        descuento = 0.10 if subtotal_iva > 50 else 0.05 if subtotal_iva > 20 else 0
+        total_final = subtotal_iva * (1-descuento)
         
-    ticket += f"\nTotal sin IVA: ${total:.2f}\n"
-    ticket += f"IVA 15%: ${iva:.2f}\n Subtotal con IVA: ${subtotal_iva:.2f}\n "
-    
-    if descuento > 0: ticket += f"Descuento: {int(descuento*100)}%\n"
-    ticket += f"TOTAL A PAGAR: ${total_final:.2f}\n{'='*40}\n"
-    print(ticket)
-    
-    registrar_venta(ticket)
-    carrito.clear()
-    guardar_carrito()
-    guardar_catalogo()
+        ticket = f"\n{'='*40}\nFACTURA \nFecha: {datetime.now()}\n {'-' * 40}\n"
+        for _, nombre, precio, cantidad in carrito:
+            subtotal_producto = precio * cantidad
+            
+            ticket += f"{nombre} x {cantidad} = ${subtotal_producto:.2f}\n "
+            
+        ticket += f"\nTotal sin IVA: ${total_sin_iva:.2f}\n"
+        ticket += f"IVA 15%: ${iva:.2f}\n Subtotal con IVA: ${subtotal_iva:.2f}\n "
         
-def registrar_venta(ticket):escribir_archivo(VENTAS_FILE, [ticket], modo='a')        
-    
+        if descuento > 0: ticket += f"Descuento: {int(descuento*100)}%\n"
+        ticket += f"TOTAL A PAGAR: ${total_final:.2f}\n{'='*40}\n"
+        print(ticket)
+        
+        c.execute("INSERT INTO ventas (ticket, fecha) VALUES (?,?)", (ticket, str(datetime.now())))
+        c.execute("DELETE FROM carrito")
+        conn.commit()
+         
 def ver_ventas():
-    if not os.path.exists(VENTAS_FILE): print("No hay ventas registradas"); return
-    ventas_todas = "".join(leer_archivo(VENTAS_FILE))
-    opcion = input("Desea ver todas las ventas o buscar una venta especifica? (todas/buscar): ").strip().lower()
-    if opcion == "todas": print(ventas_todas)
-    elif opcion == "buscar":
-        search = input("Ingrese la palabra clave para buscar (producto o fecha): ").strip().lower()
-        ventas_filtradas =[ventas for ventas in ventas_todas.split('='*40)if search in ventas.lower()]
-        print("\n".join(ventas_filtradas) if ventas_filtradas else "No se encontraron ventas con esa palabra clave. " )
-    else:
-        print("Opción inválida")
+    with conectar() as conn:
+        c = conn.cursor()
+        c.execute("SELECT ticket FROM ventas")
+        ventas = c.fetchall()
+        if not ventas: print("No hay ventas registradas"); return
+        opcion = input("Desea ver todas las ventas o buscar una venta especifica? (todas/buscar): ").strip().lower()
+        if opcion == "todas": 
+            for v in ventas: print(v[0])
+        elif opcion == "buscar":
+            search = input("Ingrese la palabra clave para buscar (producto o fecha): ").strip().lower()
+            for v in ventas:
+                if search in v[0].lower():
+                    print(v[0])  
+        else:
+            print("Opción inválida")
     
 if __name__=="__main__":
-    cargar_catalogo(); cargar_carrito()
+    inicializar_bd()
     while True:
         print("""
     \nEscoja una opción:
